fa1002436510d5ae1fe582081b00ffeba4c06fef
Chris Paschall <ChrisP1108@gmail.com>
2023-07-16 23:47:31 -0700

    Angled Text And Item Capable

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log("initialized");
+// Set Angle Styling Based On Screen Width
+
+    const html = document.querySelector("html");
+
+    function setAngle() {
+        if (window.innerWidth < 1400 && window.innerWidth >= 768) {
+            html.style.setProperty("--angle", `${window.innerWidth / 43.75}deg`);
+        } else if (window.innerWidth < 768) {
+            html.style.setProperty("--angle", `${window.innerWidth / 160}deg`);
+        } else if (window.innerWidth >= 1400) {
+            html.style.setProperty("--angle", `32deg`);
+        }
+    }
+
+    window.addEventListener("load", setAngle);
+    window.addEventListener("resize", setAngle);
+
+// Set Angled Text
+
+    const angledContainers = document.querySelectorAll(".angled-container");
+
+    angledContainers.forEach(container => {
+        const textNodes = container.querySelectorAll(".angled-text");
+        textNodes.forEach(text => {
+            const wordSplit = text.innerHTML.split(" ");
+            const addedDivs = wordSplit.map(word => 
+                `<div>${word}</div>`
+            );
+            text.innerHTML = addedDivs.join("");
+            });
+    });
